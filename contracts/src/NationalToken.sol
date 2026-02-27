11feece6314c013fa764892324f61473f43f1a05
Anthony-19 <anthonyeze248008@gmail.com>
2026-02-27 23:05:00 +0100

    FV-01 || refactor: Implemented Access control

--- contracts/src/NationalToken.sol
+++ contracts/src/NationalToken.sol
@@ -2,22 +2,39 @@
 pragma solidity ^0.8.30;
 
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
-contract NationalToken is ERC20, Ownable {
-    uint public _totalSupply;
-    address centralBank;
+import {AccessControl} from "@openzeppelin/contracts/access/AccessControl.sol";
+
+contract NationalToken is ERC20, AccessControl {
+    address private centralBank;
+
     error ZeroAmountNotAccepted();
 
-    constructor(address _centralBank) ERC20("NationalToken", "NAT") Ownable(centralBank) {
+    bytes32 public constant MINTERS_ROLE = keccak256("MINTERS_ROLE");
+
+    constructor(address _centralBank) ERC20("NationalToken", "NAT") {
+
         centralBank = _centralBank;
+
+        _grantRole(DEFAULT_ADMIN_ROLE, _centralBank);
+
+        _grantRole(MINTERS_ROLE, _centralBank);
     }
 
-    function mint(uint _amount, address _to) public onlyOwner {
+    function mint(uint _amount, address _to) public onlyRole(MINTERS_ROLE){
         if(_amount == 0){
             revert ZeroAmountNotAccepted();
         }
-        _mint(_to, _amount * (10 ** uint(decimals())));
+        _mint(_to, _amount);
     }
 
+    function setAdminRole(bytes32 role, bytes32 adminRole) external onlyRole(DEFAULT_ADMIN_ROLE){
+        _setRoleAdmin(role,adminRole);
+    }
+
+    function centralBankAddress() view returns(address){
+        return centralBank;
+    }
 }
