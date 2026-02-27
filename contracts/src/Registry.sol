2108a94adf7a7bb63eefc08cb0ff6af1d25fd8d0
Raymond Abiola <raymondintell@gmail.com>
2026-02-27 14:59:49 +0100

    FV-02 || test: Added tests for the Registry contract, fix: fixed bugs in the Registry contract

--- contracts/src/Registry.sol
+++ contracts/src/Registry.sol
@@ -3,32 +3,33 @@ pragma solidity  ^0.8.30;
 
 import {AccessControl} from "../lib/openzeppelin-contracts/contracts/access/AccessControl.sol";
 
-contract Registry is AccessControl{
+contract Registry is AccessControl {
 
-//state variables
-bytes32 public constant REGISTRATION_OFFICER_ROLE = keccak256("REGISTRATION_OFFICER_ROLE");
-address public headOfRegistrations;
+    // state variables
+    bytes32 public constant REGISTRATION_OFFICER_ROLE = keccak256("REGISTRATION_OFFICER_ROLE");
+    address public headOfRegistrations;
 
-mapping(bytes32 => bool) private isValidNIN;
-mapping(bytes32 => string) private validNameForNIN;
-mapping(bytes32 => address) private validAddressForNIN;
+    mapping(bytes32 => bool) private isValidNIN;
+    mapping(address => bool) private isValidAddress;
+    mapping(bytes32 => string) private validNameForNIN;
+    mapping(bytes32 => address) private validAddressForNIN;
 
     struct RegisteredVoter{
-        string name;
-        address voterAddress;
-        uint voterStreak;
-        bool isAddressUsed;
-        bool isRegistered;
-        bool isAccredited;
+            string name;
+            address voterAddress;
+            uint voterStreak;
+            bool isRegistered;
+            bool isAccredited;
     }
 
-    // Mappings declaration
     mapping(bytes32 => RegisteredVoter) public registeredVoter;
 
     event VoterRegisteredOnChain(bytes32 indexed ninHash, address indexed voterAddress, string indexed name);
     event VoterAddressChanged(bytes32 ninHash, address indexed oldAddress, address indexed newAddress);
 
     error ArraysNotSameLength();
+    error NINAlreadyAuthorized();
+    error AddressAlreadyAuthorized();
     error ZeroAddressNotAllowed();
     error ContractAddressNotAllowed();
     error InvalidNIN();
@@ -36,32 +37,31 @@ mapping(bytes32 => address) private validAddressForNIN;
     error AddressNotFoundInDataBase();
     error EmptyStringInputed();
     error CitizenCannotRegisterTwice();
-    error AddressAlreadyUsed();
 
     constructor(){
         headOfRegistrations = msg.sender;
         _grantRole(DEFAULT_ADMIN_ROLE, headOfRegistrations);
-        _grantRole(REGISTRATION_OFFICER, headOfRegistrations);
+        _grantRole(REGISTRATION_OFFICER_ROLE, headOfRegistrations);
     }
 
-    // authorizes batch of citizen information using saveral mappings
+    // authorizes batch of citizen information from the registration office database using saveral mappings
     function authorizeCitizensByBatch(bytes32[] calldata _ninHashes, string[] calldata _names, address[] calldata _addresses) external onlyRole(REGISTRATION_OFFICER_ROLE) {
-    if(_ninHashes.length != _names.length && _ninHashes.length != _addresses.length){
-        revert ArraysNotSameLength();
-    }
-    for(uint i = 0; i < _ninHashes.length; i++) {
-        if(_addresses[i] == address(0)){
-            revert ZeroAddressNotAllowed();
+        if(_ninHashes.length != _names.length && _ninHashes.length != _addresses.length){
+            revert ArraysNotSameLength();
         }
-        if(_addresses[i].code.length > 0){
-            revert ContractAddressNotAllowed();
+        for(uint i = 0; i < _ninHashes.length; i++) {
+            if(_addresses[i] == address(0)) revert ZeroAddressNotAllowed();
+            if(isValidNIN[_ninHashes[i]]) revert NINAlreadyAuthorized();
+            if(isValidAddress[_addresses[i]]) revert AddressAlreadyAuthorized();
+            if(_addresses[i].code.length > 0) revert ContractAddressNotAllowed();
+            isValidNIN[_ninHashes[i]] = true;
+            isValidAddress[_addresses[i]] = true;
+            validNameForNIN[_ninHashes[i]] = _names[i];
+            validAddressForNIN[_ninHashes[i]] = _addresses[i];
         }
-        isValidNIN[_ninHashes[i]] = true;
-        validNameForNIN[_ninHashes[i]] = _names[i];
-        validAddressForNIN[_ninHashes[i]] = _addresses[i];
-    }
     }
 
+    // Internal helper functions
     function getStringHash(string memory _text) internal pure returns(bytes32){
         return keccak256(abi.encodePacked(_text));
     }
@@ -70,73 +70,75 @@ mapping(bytes32 => address) private validAddressForNIN;
         return keccak256(abi.encodePacked(_num));
     }
 
-    /* voterSelfRegister is safe because if someone is aware of a citizen NIN, they do not have the 
-     private keys of the address registered in offchain database. The address are only accepted in 
-    off-chain data base after the citizen has passed KYC offchain.*/
+    /* safe voterSelfRegister even when an attacker is aware of a citizen NIN and name, they do not have the 
+    private keys of the address registered in the offchain database. An address is accepted in the off-chain database 
+    only if the citizen has passed KYC offchain.*/
     function voterSelfRegister(uint _nin, string memory _name) public {
         bytes32 ninHash = getNumHash(_nin);
         if(!isValidNIN[ninHash]){
             revert InvalidNIN();
         }
+
+        if(bytes(_name).length == 0){
+            revert EmptyStringInputed();
+        }
+
         if(getStringHash(validNameForNIN[ninHash]) != getStringHash(_name)){
             revert InvalidGovernmentRegisteredFirstName();
         }
         if(validAddressForNIN[ninHash] != msg.sender){
             revert AddressNotFoundInDataBase();
         }
-        if(bytes(_name).length == 0){
-            revert EmptyStringInputed();
-        }
+
         if(registeredVoter[ninHash].isRegistered){
             revert CitizenCannotRegisterTwice();
         }
-        if(registeredVoter[ninHash].isAddressUsed){
-            revert AddressAlreadyUsed();
-        }
-
 
         RegisteredVoter memory newVoter = RegisteredVoter({
             name: _name,
             voterAddress: msg.sender,
             voterStreak: 0,
-            isAddressUsed: true,
             isRegistered: true,
             isAccredited: false
         });
 
-        // Do we reset streak count after streak has been broken.
-
         registeredVoter[ninHash] = newVoter;
         emit VoterRegisteredOnChain(ninHash,msg.sender, _name);
-        }
+    }
 
-        /* Use function in the case when a voter misplaces their private keys for oldAddress.
-         Citizen has to go to voter registration office to make complaint and provide a new address 
-         that will replace the old address. 
-         Registration_officer can only call this function after doing kyc checks off-chain. */
-        function changeVoterAddress(uint _nin, address _newAddress)external onlyRole(REGISTRATION_OFFICER_ROLE){
+    /* Use function in the case when a voter misplaces their private keys for oldAddress.
+        The citizen must go to the voter registration office to issue a complaint and provide a new address 
+        that will replace the compromised address. 
+        Registration_officer can only call this function after doing kyc checks off-chain. */
+    function changeVoterAddress(uint _nin, address _newAddress)external onlyRole(REGISTRATION_OFFICER_ROLE){
         bytes32 ninHash = getNumHash(_nin);
         
-        if(_newAddress == address(0)){
-            revert ZeroAddressNotAllowed();
-        }
-        if(_newAddress.code.length > 0){
-            revert ContractAddressNotAllowed();
-        }
-        if(!isValidNIN[ninHash]){
-            revert InvalidNIN();
-        }
+        if(isValidAddress[_newAddress]) revert AddressAlreadyAuthorized();
+        if(_newAddress == address(0)) revert ZeroAddressNotAllowed();
+        if(_newAddress.code.length > 0) revert ContractAddressNotAllowed();
+        if(!isValidNIN[ninHash]) revert InvalidNIN();
     
-        address oldAddress = registeredVoter[ninHash].voterAddress;
+        address compromisedAddress = registeredVoter[ninHash].voterAddress;
         registeredVoter[ninHash].voterAddress = _newAddress;
-        emit VoterAddressChanged(ninHash, oldAddress, _newAddress);
-        }
+        isValidAddress[_newAddress] = true;
+        validAddressForNIN[ninHash] = _newAddress;
+        emit VoterAddressChanged(ninHash, compromisedAddress, _newAddress);
+    }
 
-        function getVoterRegistrationData(uint _nin) external view returns(RegisteredVoter memory){
+    // View Functions
+    function getVoterRegistrationData(uint _nin) external view returns(RegisteredVoter memory){
             return registeredVoter[getNumHash(_nin)];
-        }
+    }
 
-        function getValidityOfNIN(uint _nin) external view returns(bool){
-            return isValidNIN[getNumHash(_nin)];
-        }
+    function getValidityOfNIN(uint _nin) external view returns(bool){
+        return isValidNIN[getNumHash(_nin)];
+    }
+
+    function getValidityOfAddress(address _address) external view returns(bool){
+        return isValidAddress[_address];
+    }
+
+    function getValidAddressForNIN(uint _nin) external view returns(address){
+        return validAddressForNIN[getNumHash(_nin)];
+    }
 }
--- contracts/test/Registry.t.sol
+++ contracts/test/Registry.t.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.30;
 
 import "forge-std/Test.sol";
 import {IAccessControl} from "@openzeppelin/contracts/access/AccessControl.sol";
@@ -15,10 +15,23 @@ contract RegistryTest is Test {
     address public user1;
     address public user2;
     address public user3;
+    address public user4;
 
     bytes32[] ninHashes;
     string[] names;
     address[] addresses;
+
+    error ArraysNotSameLength();
+    error NINAlreadyAuthorized();
+    error AddressAlreadyAuthorized();
+    error ZeroAddressNotAllowed();
+    error ContractAddressNotAllowed();
+    error InvalidNIN();
+    error InvalidGovernmentRegisteredFirstName();
+    error AddressNotFoundInDataBase();
+    error EmptyStringInputed();
+    error CitizenCannotRegisterTwice();
+    error AccessControlUnauthorizedAccount(address account, bytes32 neededRole);
 
 function setUp() public {
     owner = address(this);
@@ -26,6 +39,7 @@ function setUp() public {
     user1 = vm.addr(pKey);
     user2 = makeAddr("user2");
     user3 = vm.addr(pKey2);
+    user4 = makeAddr("user4");
 
     registry = new Registry();
 }
@@ -43,6 +57,9 @@ function getNumHash(uint _num) internal pure returns(bytes32){
 }
 
 function simulateArrayPopulate() internal {
+ninHashes = new bytes32[](3);
+names = new string[](3);
+addresses = new address[](3);
 ninHashes[0] = getNumHash(2345);
 ninHashes[1] = getNumHash(3388);
 ninHashes[2] = getNumHash(8871);
@@ -54,29 +71,106 @@ names[2] = "Ben";
 addresses[0] = user1;
 addresses[1] = user2;
 addresses[2] = user3;
-vm.prank(owner);
-registry.authorizeCitizensByBatch(ninHashes, names, addresses);
 
+registry.authorizeCitizensByBatch(ninHashes, names, addresses);
 }
 
 function testAuthorizeCitizensByBatch() public {
-
 simulateArrayPopulate();
 assertTrue(registry.getValidityOfNIN(2345));
 assertTrue(registry.getValidityOfNIN(3388));
 assertTrue(registry.getValidityOfNIN(8871));
-
+assertEq(registry.getValidAddressForNIN(8871), user3);
+assertTrue(registry.getValidityOfAddress(user2));
 }
 
 function testVoterSelfRegister() public {
+simulateArrayPopulate();
+
+// Edge case test for a valid citizen tries to register with an Invalid NIN
+vm.prank(user1);
+vm.expectRevert(InvalidNIN.selector);
+registry.voterSelfRegister(2346, "Alice");
+
+//Edge case test for when a valid citizen tries to register with an Invalid government registered first name 
+vm.prank(user1);
+vm.expectRevert(InvalidGovernmentRegisteredFirstName.selector);
+registry.voterSelfRegister(2345, "Aliee");
+
+// Edge case test for a wrong address trying to register with a valid voter information leak.
+vm.prank(user4);
+vm.expectRevert(AddressNotFoundInDataBase.selector);
+registry.voterSelfRegister(2345, "Alice");
+
+// Edge case for when a valid citizen tries to register with an empty string.
+vm.prank(user1);
+vm.expectRevert(EmptyStringInputed.selector);
+registry.voterSelfRegister(2345, "");
+
+// Test if data of successful registration is correct.
+vm.prank(user1);
+registry.voterSelfRegister(2345, "Alice");
+assertEq(registry.getVoterRegistrationData(2345).voterAddress, user1);
+assertEq(registry.getVoterRegistrationData(2345).voterStreak, 0);
+assertTrue(registry.getVoterRegistrationData(2345).isRegistered);
+assertFalse(registry.getVoterRegistrationData(2345).isAccredited);
+
+// Edge case for when a valid citizen tries to register twice.
+vm.prank(user1);
+vm.expectRevert(CitizenCannotRegisterTwice.selector);
+registry.voterSelfRegister(2345, "Alice");
 
 }
 
-function testGetVoterRegistrationData() public {
+function testChangeVoterAddress() public {
+    simulateArrayPopulate();
+    bytes32 registrationOfficer = registry.REGISTRATION_OFFICER_ROLE();
 
+    vm.prank(user1);
+    registry.voterSelfRegister(2345, "Alice");
+
+    // Edge case test for when a user without the registration officer role tries to call the changeVoterAddress function
+    vm.prank(user2);
+    vm.expectRevert(
+        abi.encodeWithSelector(
+            AccessControlUnauthorizedAccount.selector,
+            user2,
+            registrationOfficer
+        )
+    );
+    registry.changeVoterAddress(2345, user4);
+
+    registry.grantRole(registrationOfficer, user3);
+
+    // Edge case test for when a registration officer tries to change voter address to an already Authorized Address.
+    vm.startPrank(user3);
+    vm.expectRevert(AddressAlreadyAuthorized.selector);
+    registry.changeVoterAddress(2345, user1);
+
+    // Edge case for when a zero address is passed into the change voter address function
+    vm.expectRevert(ZeroAddressNotAllowed.selector);
+    registry.changeVoterAddress(2345, zeroAddress);
+
+    // Edge case for when a contract address is passed into the change voter address function
+    vm.expectRevert(ContractAddressNotAllowed.selector);
+    registry.changeVoterAddress(2345, address(registry));
+
+    // Edge case for when an invalid NIN is passed into the change voter address function
+    vm.expectRevert(InvalidNIN.selector);
+    registry.changeVoterAddress(2346, user4);
+
+    // Tests for when the changeVoterAddres function is correctly called
+    registry.changeVoterAddress(2345, user4);
+    assertEq(registry.getVoterRegistrationData(2345).voterAddress, user4);
+    assertTrue(registry.getValidityOfAddress(user4));
+    assertEq(registry.getValidAddressForNIN(2345), user4);
+    vm.stopPrank();
 }
 
-function testGetValidityOfNIN() public {
-
+function testGrantRegistrationOfficerRole() public {
+    bytes32 registrationOfficer = registry.REGISTRATION_OFFICER_ROLE();
+    assertFalse(registry.hasRole(registrationOfficer, user1));
+    registry.grantRole(registrationOfficer, user1);
+    assertTrue(registry.hasRole(registrationOfficer, user1));
 }
 }
