615fb61e174ad49e7e49d0180d0798aad68910d9
Raymond Abiola <raymondintell@gmail.com>
2026-03-01 19:02:28 +0100

    FV-07 || add: added ipfs link for token URI link

--- contracts/src/DemocracyBadge.sol
+++ contracts/src/DemocracyBadge.sol
@@ -6,42 +6,46 @@ import "@openzeppelin/contracts/access/AccessControl.sol";
 
 contract DemocracyBadge is ERC721, AccessControl{
 uint tokenId_;
-/* assign role to Elections contract. If a voter votes for a candidate and their voter streak gets
+/* assign role to Elections contract. If a voter votes in an election and their voter streak gets
   to the threshold, then the Elections contract will call the mintDemocracyBadge function to voter address*/
 bytes32 public constant ELECTIONS_CONTRACT_ROLE = keccak256("ELECTIONS_CONTRACT_ROLE");
 
 error AlreadyHasDemocracyBadge();
 error SoulBoundToken();
-    constructor(string memory _name, string memory _symbol)
-        ERC721(_name, _symbol){
-        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
-        }
 
-    function mintDemocracyBadge(address to) external onlyRole(ELECTIONS_CONTRACT_ROLE) {
-    require(balanceOf(to) == 0, AlreadyHasDemocracyBadge());
+constructor(string memory _name, string memory _symbol) ERC721(_name, _symbol){
+    _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+}
+
+function mintDemocracyBadge(address to) external onlyRole(ELECTIONS_CONTRACT_ROLE) {
+    if(balanceOf(to) != 0) revert AlreadyHasDemocracyBadge();
     ++tokenId_;
     _safeMint(to, tokenId_);
-    }
+}
 
-    function _update(address to, uint tokenId, address auth) internal override returns(address){
-        address from = _ownerOf(tokenId);
+function _update(address to, uint tokenId, address auth) internal override returns(address){
+    address from = _ownerOf(tokenId);
 
-        if(from != address(0)){
+    if(from != address(0)){
             revert SoulBoundToken();
-        }
+    }
     return super._update(to, tokenId, auth);
-    }
+}
 
-    function approve(address, uint256) public pure override {
+function approve(address, uint256) public pure override {
     revert SoulBoundToken();
-    }
+}
 
-    function setApprovalForAll(address, bool) public pure override {
+function setApprovalForAll(address, bool) public pure override {
     revert SoulBoundToken();
-    }
+}
 
-    function supportsInterface(bytes4 interfaceId) public view virtual override(ERC721, AccessControl) returns (bool){
+function tokenURI(uint256) public view virtual override returns (string memory){
+    return "ipfs://bafkreidbix2i3gnhujrmxccprz4bqjbhuy24rfhzdawdfihtni5tg5b3ba";
+}
+
+function supportsInterface(bytes4 interfaceId) public view virtual override(ERC721, AccessControl) returns (bool){
     return super.supportsInterface(interfaceId);
-    }
+}
 
 }
