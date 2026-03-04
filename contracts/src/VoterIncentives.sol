d04a348f0978ff79c171ea411d722b29e8d458d0
Anthony-19 <anthonyeze248008@gmail.com>
2026-03-04 12:44:41 +0100

    FV-06 || fix || refactor my code

--- contracts/src/VoterIncentives.sol
+++ contracts/src/VoterIncentives.sol
@@ -3,49 +3,73 @@ pragma solidity ^0.8.30;
 import {DemocracyBadge} from "src/DemocracyBadge.sol";
 import {NationalToken} from "src/NationalToken.sol";
 import {Registry} from "src/Registry.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
-contract VoterIncentives {
+contract VoterIncentives is Ownable {
+
     DemocracyBadge public democracyBadge;
 
     NationalToken public nationalToken;
 
     Registry public registry;
 
-    error Invalid_Address();
-    error No_VoterStreak();
-    error Not_Eligible_Yet();
-    error Already_Claim_Streak();
-    error Dont_Have_DemocracyBadge();
+    uint incentiveAmount;
+
+    error InvalidAddress();
+    error NoVoterStreak();
+    error NotEligibleYet();
+    error AlreadyClaimedIncentiveForCurrentStreakThreshold(uint currentStreak);
+    error DontHaveDemocracyBadge();
+    error ContractAddressNotAllowed(address caller);
 
     mapping(address => mapping(uint => bool)) public hasClaimedIncentive;
+
+    constructor ( address _democracyBadge,
+    address _nationalToken,
+    address _registry,
+    uint _incentiveAmount){
+        democracyBadge = DemocracyBadge(tokenAddress);
+        nationalToken = NationalToken(tokenAddress);
+        registry = Registry(tokenAddress);
+        incentiveAmount = _incentiveAmount;
+    }
+
+    function checkEligibility(address _address,uint _voterStreak) internal {
+          if (_address == address(0)) {
+            revert(InvalidAddress());
+        }
+
+        if(_address.code.length > 0){
+               revert ContractAddressNotAllowed(_address);
+        }
+
+
+        if (_voterStreak % 3 != 0) {
+            revert(NotEligibleYet());
+        }
+
+        if (hasClaimedIncentive[_address][_voterStreak]) {
+            revert(AlreadyClaimedIncentiveForCurrentStreakThreshold(_voterStreak));
+        }
+
+        if (democracyBadge.balanceOf(_address) == 0) {
+            revert(DontHaveDemocracyBadge());
+        }
+    }
 
     function claimIncentives() external {
         uint voterStreak = registry
             .getVoterDataViaAddress(msg.sender)
             .voterStreak;
 
-        if (msg.sender == address(0)) {
-            revert(Invalid_Address());
-        }
-
-        if (voterStreak < 1) {
-            revert(No_VoterStreak());
-        }
-
-        if (voterStreak % 3 != 0) {
-            revert(Not_Eligible_Yet());
-        }
-
-        if (hasClaimedIncentive[msg.sender][voterStreak]) {
-            revert(Already_Claim_Streak());
-        }
-
-        if (democracyBadge.balanceOf(msg.sender) == 0) {
-            revert(Dont_Have_DemocracyBadge());
-        }
+        checkEligibility(msg.sender, voterStreak);
 
         hasClaimedIncentive[msg.sender][voterStreak] = true;
 
-        nationalToken.mint(msg.sender, 100);
+        nationalToken.mint(msg.sender, incentiveAmount);
+    }
+
+   function setCurrentIncentive(uint _incentiveAmount) external onlyOwner {
+          incentiveAmount = _incentiveAmount;  
     }
 }
