7f85b6ca2b517c0d5b5f2c54c5a76cbd2d85ae67
Raymond Abiola <raymondintell@gmail.com>
2026-03-13 15:25:21 +0100

    FV-01 || refactor: refactored national token contract and test file

--- contracts/test/NationalToken.t.sol
+++ contracts/test/NationalToken.t.sol
@@ -7,72 +7,57 @@ import {NationalToken} from "src/NationalToken.sol";
 
 contract NationalTokenTest is Test {
     NationalToken public nationalToken;
-    address admin;
+    address centralBank;
+    address user1;
+    address zeroAddress;
+
+    error InvalidAddress();
+    error InvalidAmount();
+    error ContractAddressNotAllowed();
+    error AccessControlUnauthorizedAccount(address account, bytes32 neededRole);
 
     function setUp() public {
-        address _admin = makeAddr("1");
-        admin = _admin;
-        nationalToken = new NationalToken(admin);
+        centralBank = makeAddr("1");
+        user1 = makeAddr("2");
+        zeroAddress = address(0);
+        nationalToken = new NationalToken(centralBank);
     }
 
-    function testGetAddress() public view {
-        address centralBank = nationalToken.centralBankAddress();
-        console.log("Central Bank Address from test:", centralBank);
+    function testIfDeploymentWorkedCorrectly() public {
+        bytes32 defaultAdmin = nationalToken.DEFAULT_ADMIN_ROLE();
+        bytes32 minterRole = nationalToken.MINTER_ROLE();
+        assertEq(nationalToken.centralBank(), centralBank);
+        assertTrue(nationalToken.hasRole(defaultAdmin, centralBank));
+        assertTrue(nationalToken.hasRole(minterRole, centralBank));
+        assertGt(address(nationalToken).code.length, 0);
+    }
+
+    function testDeploymentEdgeCases() public {
+        vm.expectRevert(InvalidAddress.selector);
+        nationalToken = new NationalToken(zeroAddress);
+
+        vm.expectRevert(ContractAddressNotAllowed.selector);
+        nationalToken = new NationalToken(address(this));
     }
 
     function testMint() public {
-        address recipient = makeAddr("owner");
-
         uint mintAmount = 1000e18;
-
-        vm.prank(admin);
-
-        nationalToken.mint(recipient, 1000e18);
-
-        assertEq(nationalToken.balanceOf(recipient), mintAmount);
-    }
-
-    function testGrantRole() public {
-        vm.startPrank(admin);
-        address users = makeAddr("3");
-        nationalToken.grantRole(nationalToken.MINTERS_ROLE(), users);
-        vm.stopPrank;
-        vm.prank(users);
-        bool hasMinterRole = nationalToken.hasRole(
-            nationalToken.MINTERS_ROLE(),
-            users
+        bytes32 minterRole = nationalToken.MINTER_ROLE();
+        vm.expectRevert(
+            abi.encodeWithSelector(
+                AccessControlUnauthorizedAccount.selector,
+                address(this),
+                minterRole
+            )
         );
-        assertTrue(hasMinterRole);
-    }
+        nationalToken.mint(user1, mintAmount);
 
-    function testUserCanMint() public {
-        address user = makeAddr("4");
-
-        vm.startPrank(admin);
-
-        nationalToken.grantRole(nationalToken.MINTERS_ROLE(), user);
-
+        vm.startPrank(centralBank);
+        vm.expectRevert(InvalidAmount.selector);
+        nationalToken.mint(user1, 0);
+        nationalToken.mint(user1, mintAmount);
         vm.stopPrank();
 
-        vm.prank(user);
-
-        nationalToken.mint(user, 100);
-
-        assertEq(nationalToken.balanceOf(user), 100);
-    }
-
-    function testSetAdminRole() public {
-        vm.startPrank(admin);
-        nationalToken.setAdminRole(
-            nationalToken.MINTERS_ROLE(),
-            nationalToken.DEFAULT_ADMIN_ROLE()
-        );
-
-        vm.stopPrank();
-
-        assertEq(
-            nationalToken.getRoleAdmin(nationalToken.MINTERS_ROLE()),
-            nationalToken.DEFAULT_ADMIN_ROLE()
-        );
+        assertEq(nationalToken.balanceOf(user1), mintAmount);
     }
 }
