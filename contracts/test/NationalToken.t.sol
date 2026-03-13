491c9386950b166fae2bdc3c60b9c06881223b2a
Raymond Abiola <60116085+raymondabiola@users.noreply.github.com>
2026-03-13 15:27:36 +0100

    Fv 01 bi implement national token (#30)
    * FV-01 || add: added skeleton for national token test
    
    * FV-01 || refactor: refactored national token contract and test file

---
+++ contracts/test/NationalToken.t.sol
@@ -0,0 +1,63 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.30;
+
+import {Test, console} from "forge-std/Test.sol";
+import {NationalToken} from "src/NationalToken.sol";
+
+contract NationalTokenTest is Test {
+    NationalToken public nationalToken;
+    address centralBank;
+    address user1;
+    address zeroAddress;
+
+    error InvalidAddress();
+    error InvalidAmount();
+    error ContractAddressNotAllowed();
+    error AccessControlUnauthorizedAccount(address account, bytes32 neededRole);
+
+    function setUp() public {
+        centralBank = makeAddr("1");
+        user1 = makeAddr("2");
+        zeroAddress = address(0);
+        nationalToken = new NationalToken(centralBank);
+    }
+
+    function testIfDeploymentWorkedCorrectly() public {
+        bytes32 defaultAdmin = nationalToken.DEFAULT_ADMIN_ROLE();
+        bytes32 minterRole = nationalToken.MINTER_ROLE();
+        assertEq(nationalToken.centralBank(), centralBank);
+        assertTrue(nationalToken.hasRole(defaultAdmin, centralBank));
+        assertTrue(nationalToken.hasRole(minterRole, centralBank));
+        assertGt(address(nationalToken).code.length, 0);
+    }
+
+    function testDeploymentEdgeCases() public {
+        vm.expectRevert(InvalidAddress.selector);
+        nationalToken = new NationalToken(zeroAddress);
+
+        vm.expectRevert(ContractAddressNotAllowed.selector);
+        nationalToken = new NationalToken(address(this));
+    }
+
+    function testMint() public {
+        uint mintAmount = 1000e18;
+        bytes32 minterRole = nationalToken.MINTER_ROLE();
+        vm.expectRevert(
+            abi.encodeWithSelector(
+                AccessControlUnauthorizedAccount.selector,
+                address(this),
+                minterRole
+            )
+        );
+        nationalToken.mint(user1, mintAmount);
+
+        vm.startPrank(centralBank);
+        vm.expectRevert(InvalidAmount.selector);
+        nationalToken.mint(user1, 0);
+        nationalToken.mint(user1, mintAmount);
+        vm.stopPrank();
+
+        assertEq(nationalToken.balanceOf(user1), mintAmount);
+    }
+}
