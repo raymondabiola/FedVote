7d28ca5b97f823aa8afb13fe1826777383f7c74a
Raymond Abiola <raymondintell@gmail.com>
2026-03-19 01:31:42 +0100

    modified database

--- contracts/script/AuthorizeCitizens.s.sol
+++ contracts/script/AuthorizeCitizens.s.sol
@@ -5,36 +5,45 @@ import "forge-std/Script.sol";
 import "../src/Registry.sol";
 
 contract RegisterVoters is Script {
-  Registry public registry ;
+    Registry public registry;
 
     function run() external {
-        uint256 callerPrivateKey = vm.envUint("PRIVATE_KEY");
+        uint256 callerPrivateKey = vm.envUint("DEPLOYER_PRIVATE_KEY");
         vm.startBroadcast(callerPrivateKey);
 
         address registryAddr = vm.envAddress("REGISTRY_ADDRESS");
-
         registry = Registry(registryAddr);
 
-        string memory json = vm.readFile("./data/voters.json");
+        string memory json = vm.readFile("../data/database.json");
+        
+        // Count array elements by trying to parse each index until it fails
+        uint256 count = 0;
+        while (true) {
+            try vm.parseJsonAddress(json, string.concat("[", vm.toString(count), "].address")) {
+                count++;
+            } catch {
+                break;
+            }
+        }
 
-        uint256 count = vm.parseJsonUint(json, ".length");
-
-       bytes32[] memory ninHashes = new bytes32[](count);
-       string[] memory names = new string[](count);
-       address[] memory addresses = new address[](count);
+        bytes32[] memory ninHashes = new bytes32[](count);
+        string[] memory names = new string[](count);
+        address[] memory addresses = new address[](count);
 
         for (uint256 i = 0; i < count; i++) {
-            string memory path = string.concat(".", vm.toString(i));
+            string memory basePath = string.concat("[", vm.toString(i), "]");
+            
+            string memory nin = vm.parseJsonString(json, string.concat(basePath, ".nin"));
+            string memory name = vm.parseJsonString(json, string.concat(basePath, ".name"));
+            address voterAddress = vm.parseJsonAddress(json, string.concat(basePath, ".address"));
 
-            string memory nin = vm.parseJsonString(json, string.concat(path, ".nin"));
-            string memory name = vm.parseJsonString(json, string.concat(path, ".name"));
-            address voterAddress = vm.parseJsonAddress(json, string.concat(path, ".address"));
+            uint ninUint = vm.parseUint(nin);
 
-            bytes32 ninHash = keccak256(abi.encodePacked(nin));
+            bytes32 ninHash = keccak256(abi.encodePacked(ninUint));
 
             ninHashes[i] = ninHash;
             names[i] = name;
-        addresses[i] = voterAddress;
+            addresses[i] = voterAddress;
         }
 
         bytes32 role = registry.REGISTRATION_OFFICER_ROLE();
